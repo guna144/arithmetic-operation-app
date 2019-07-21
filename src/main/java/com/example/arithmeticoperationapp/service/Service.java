6b490c3d39b2d8975f9c6aaac9ed22d1630e9b11
guna144 <gunaapps14@gmail.com>
2019-07-21 10:35:58 +0530

    src upload

---
+++ src/main/java/com/example/arithmeticoperationapp/service/Service.java
@@ -0,0 +1,25 @@
+package com.example.arithmeticoperationapp.service;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Service {
+
+	
+	@RequestMapping(path="/add/{input1}/{input2}")
+	private Integer add(@PathVariable("input1") int input1, 
+	        @PathVariable("input2") int input2) {
+
+		return input1 + input2;
+	}
+	
+	@GetMapping("/test")
+	private String test() {
+		return "Testing done!";
+	}
+}
